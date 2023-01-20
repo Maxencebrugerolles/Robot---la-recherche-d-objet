d5cdb358fd496260d094f245e03b4630c62abbb9
EvanLorut <122687159+EvanLorut@users.noreply.github.com>
2023-01-20 15:20:29 +0100

    Update SuiviMurlimotp.py

--- SuiviMurlimotp.py
+++ SuiviMurlimotp.py
@@ -27,38 +27,42 @@ def callback_laser(laser, odometry):
 
     d=0.2 #distance acceptable pour juger que la voie est libre
     
-    a=laser.range[0]
+    #on définit les extremums des lasers du lidar ainsi que le milieu
+    a=laser.range[0] 
     b=laser.range[8]
     c=laser.range[15]
     
+    #On créé une boucle logique pour que le robot sache où se déplacer 
     while a>0 && b>0 && c>0:
-        if a>=d:
-            move_cmd.angular.z=-1,5708
-            time.sleep(1000)
-            move_cmd.angular.z=0
-            move_cmd.linear.x=0.1
-            time.sleep(1000)
-            move_cmd.linear.x=0
-        else:
-            if b>=d:
-                move_cmd.linear.x=0.1
-                time.sleep(1000)
-                move_cmd.linear.x=0
-            else:
-                if a>=d:
-                    move_cmd.angular.z=1,5708
-                    time.sleep(1000)
-                    move_cmd.angular.z=0
-                    move_cmd.linear.x=0.1
-                    time.sleep(1000)
-                    move_cmd.linear.x=0
-                else:
-                    move_cmd.angular.z=3,141592
+        if a>=d:                                            #On vérifie que le robot ait la place de se déplacer à droite 
+            move_cmd.angular.z=-1,5708 #
+            time.sleep(1000)           #
+            move_cmd.angular.z=0       # si oui
+            move_cmd.linear.x=0.1      #
+            time.sleep(1000)           #
+            move_cmd.linear.x=0        #
+        else:                                               #si non
+            if b>=d:                                        #On vérifie que le robot ait la place de se déplacer au centre
+                move_cmd.linear.x=0.1 #
+                time.sleep(1000)      #si oui
+                move_cmd.linear.x=0   #
+            else:                                            #si non
+                if a>=d:                                     #On vérifie que le robot ait la place de se déplacer à gauche
+                    move_cmd.angular.z=1,5708 #
+                    time.sleep(1000)          #
+                    move_cmd.angular.z=0      #
+                    move_cmd.linear.x=0.1     #si oui
+                    time.sleep(1000)          #
+                    move_cmd.linear.x=0       #
+                else:                                        #si non on fait un demi tour
+                    move_cmd.angular.z=3,141592 
                     time.sleep(1000)
                     move_cmd.angular.z=0
                 end
             end
         end
+    end
+        
         
         
     pub.publish(move_cmd)
