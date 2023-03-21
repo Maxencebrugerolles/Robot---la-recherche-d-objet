61ad4df8a45b12763439b4097e7507d12d9794b9
Maxencebrugerolles <46576605+Maxencebrugerolles@users.noreply.github.com>
2023-03-21 12:12:55 +0100

    Update SuiviMur.py

--- projetpeip/scripts/SuiviMur.py
+++ projetpeip/scripts/SuiviMur.py
@@ -43,12 +43,15 @@ def callback_laser(laser, odometry):
     if c>0.2:
         move_cmd.angular.z=a
         move_cmd.linear.x=0.1
+        disp("a")
         if b==0.2:
             move_cmd.angular.z=-a
             move_cmd.linear.x=0.1
+            disp("b")
     if c<0.2:
         move_cmd.angular.z=-a
         move_cmd.linear.x=0.1
+        disp("c")
   
         
     #pub.publish(move_cmd) 
