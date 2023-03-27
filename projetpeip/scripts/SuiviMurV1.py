ca4ca5e0dd1aaa828a91e633e42baef3fb900b96
Maxencebrugerolles <46576605+Maxencebrugerolles@users.noreply.github.com>
2023-03-27 17:18:26 +0200

    Update SuiviMurV1.py

--- projetpeip/scripts/SuiviMurV1.py
+++ projetpeip/scripts/SuiviMurV1.py
@@ -27,32 +27,31 @@ def callback_laser(laser, odometry):
     
     
 
-    d=0.2                                                   #distance acceptable pour juger que la voie est libre
     
-                                                            #on definit les extremums des lasers du lidar ainsi que le milieu
-    a=laser.ranges[0] 
+    
+                      
+    a=laser.ranges[0]                                       #on definit les extremums des lasers du lidar ainsi que le milieu
     b=laser.ranges[8]
     c=laser.ranges[15]
+    
+    d=0.2                                                   #distance acceptable pour juger que la voie est libre
     
     print("a = ",a)
     print("b = ",b)
     print("c = ",c)
-    print("d = ",d)
 
-    if a>0 and b>0 and c>0:                         #On cree une boucle logique pour que le robot sache ou se deplacer 
-        if a>=d:                                            #On verifie que le robot a la place de se deplacer a droite 
-            move_cmd.angular.z=1,57                             # si oui                                  
-            move_cmd.linear.x=0.1                               # 
-        else:                                                   #si non        
-            if b>=d:                                        #On verifie que le robot a la place de se deplacer au centre
-                move_cmd.linear.x=0.1                           #si oui
-
-            else:                                               #si non
-                if a>=d:                                    #On verifie que le robot a la place de se deplacer a gauche
-                    move_cmd.angular.z=-1,57                      #si oui
-                    move_cmd.linear.x=0.1                       #
-                else:                                       #si non on fait un demi tour
-                    move_cmd.angular.z=3,14
+    if a>=d:                                            #On verifie que le robot a la place de se deplacer a droite 
+        move_cmd.angular.z=1,57                             # si oui                                  
+        move_cmd.linear.x=0.1                               # 
+    else:                                                   #si non        
+        if b>=d:                                        #On verifie que le robot a la place de se deplacer au centre
+            move_cmd.linear.x=0.1                           #si oui
+        else:                                               #si non
+            if c>=d:                                    #On verifie que le robot a la place de se deplacer a gauche
+                move_cmd.angular.z=-1,57                      #si oui
+                move_cmd.linear.x=0.1                       #
+            else:                                       #si non on fait un demi tour
+                move_cmd.angular.z=3,14
 
         
        
